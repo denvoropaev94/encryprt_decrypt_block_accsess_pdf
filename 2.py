4c3bac04ccfd446ef8facb66e6b738d2b2424589
«Denis» <denvoropaev7@gmail.com>
2023-03-17 16:02:39 +0300

    encrypt and decrypt files, as well as block access to the contents of a PDF file using a password

---
+++ 2.py
@@ -0,0 +1,14 @@
+from PyPDF2 import PdfWriter,PdfReader
+from getpass import getpass
+
+pdfwriter = PdfWriter()
+pdf = PdfReader('leetcode_python.pdf')
+
+for page in range(len(pdf.pages)):
+    pdfwriter.add_page(pdf.pages[page])
+
+password = getpass(prompt = 'Введите пароль: ')
+pdfwriter.encrypt(password)
+
+with open('protected.pdf','wb') as file:
+    pdfwriter.write(file)
